cd91a05352260743679f64890e7d01efda8f9b5e
ABelliqueux <contact@arthus.net>
2021-05-21 18:00:26 +0200

    Remove pandoc dep for one.md g8n

--- hooks.py
+++ hooks.py
@@ -1,14 +1,44 @@
-import subprocess
+import os
 import fileinput
 
-# ~ def build_onepage(*args, **kwargs):
-def build_onepage(*args, **kwargs):
-    subprocess.call( [ "mkdocs2pandoc","-f","mkdocs.yml","-o","docs/one.md" ] )
-    
+ROOT_FOLDER = "docs"
 
-    # Remove lines 6 to 22 and replace '#' with '##' for a nice toc generation
-    with fileinput.FileInput("docs/one.md", inplace=True) as infile:
-        for line in infile:
-            if infile.lineno() > 6 and infile.lineno() < 22 :
-                continue
-            print( (line.replace('##', '#') ).replace( '#', '##' ) , end='')
+def build_onepage(*args, **kwargs):
+
+    files = os.listdir(ROOT_FOLDER)
+    
+    files.sort()
+
+    # Remove index.md file
+
+    files.remove('index.md')
+
+    # If exists, remove one.md to avoid recursion loop
+    
+    if 'one.md' in files:
+        
+        files.remove('one.md')
+
+    with open( ROOT_FOLDER + os.sep + "one.md", "w") as outfile:
+        
+        for file in files :
+        
+            # Only use MD files
+        
+            if file.endswith('.md'):
+        
+                # Replace '_' with ',' except if last occurence, then '&'.
+        
+                cleanName = file[:-3].replace('_',', ').capitalize().rsplit(', ',1)
+                
+                amp = " & "
+        
+                # Add header markup + linebreaks
+        
+                outfile.write("\n## " + amp.join( cleanName ) + "\n\n")
+        
+                with open(ROOT_FOLDER + os.sep + file) as mdfile:
+        
+                    for line in mdfile:
+                        
+                        outfile.write( line ) 
